d492750a5df61ffec16273eb9b32bc49abf32754
zhaoshuxue <zhaoshuxue@163.com>
2017-03-30 19:57:11 +0800

    commitByZsx

--- spring-boot-cloud-config-server/src/main/java/com/zsx/App.java
+++ spring-boot-cloud-config-server/src/main/java/com/zsx/App.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
+@EnableConfigServer // 开启Config Server
 @SpringBootApplication
 public class App {
 	public static void main(String[] args) {
---
+++ zsx.py
@@ -0,0 +1,21 @@
+import os
+
+# os.chdir('E:/git_repo/springBoot') 
+
+os.system("git status")
+
+os.system("git pull")
+
+os.system("git add .")
+
+os.system("git commit -m commitByZsx")
+
+os.system("git push -u origin master")
+
+# os.system("pause")
+
+print("---------------------------------------------------------------")
+print("commit success !!! ok !")
+print("---------------------------------------------------------------")
+
+os.system('ping -n 5 127.1>nul')
