9548f9dfe9159527d96156a3004159b5dcd61f3b
zhaoshuxue <634790417@qq.com>
2017-04-02 23:20:14 +0800

    commitByZsx

--- spring-boot-registry/src/main/java/com/zsx/controller/TestController.java
+++ spring-boot-registry/src/main/java/com/zsx/controller/TestController.java
@@ -1,11 +1,11 @@
 package com.zsx.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+//import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RefreshScope
+//@RefreshScope
 @RestController
 public class TestController {
 
