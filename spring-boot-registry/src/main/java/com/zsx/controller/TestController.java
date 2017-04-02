db2ec2e899d509ad49f785f1bcfdd5b7e306bff8
zhaoshuxue <zhaoshuxue@163.com>
2017-04-03 02:39:46 +0800

    commitByZsx

--- spring-boot-registry/src/main/java/com/zsx/controller/TestController.java
+++ spring-boot-registry/src/main/java/com/zsx/controller/TestController.java
@@ -1,19 +1,19 @@
-package com.zsx.controller;
-
-import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-//@RefreshScope
-@RestController
-public class TestController {
-
-	@Value("${from}")
-	private String from;
-
-	@RequestMapping("/from")
-	public String from() {
-		return this.from;
-	}
-}
+//package com.zsx.controller;
+//
+//import org.springframework.beans.factory.annotation.Value;
+////import org.springframework.cloud.context.config.annotation.RefreshScope;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+////@RefreshScope
+//@RestController
+//public class TestController {
+//
+//	@Value("${from}")
+//	private String from;
+//
+//	@RequestMapping("/from")
+//	public String from() {
+//		return this.from;
+//	}
+//}
