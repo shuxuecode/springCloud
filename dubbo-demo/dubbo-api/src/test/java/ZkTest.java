1bf6969e135e2000d1ed7fca5c6b000522ef89f8
zhaoshuxue <zhaoshuxue@163.com>
2017-04-05 23:57:40 +0800

    commitByZsx

--- dubbo-demo/dubbo-api/src/test/java/ZkTest.java
+++ dubbo-demo/dubbo-api/src/test/java/ZkTest.java
@@ -9,17 +9,21 @@ public class ZkTest {
 
 	public static void main(String[] args) throws Exception {
 
-		ZooKeeper zk = new ZooKeeper("127.0.0.1:2181", 60000, new Watcher() {
+		ZooKeeper zk = new ZooKeeper("localhost:2181", 6000, new Watcher() {
 
 			public void process(WatchedEvent event) {
 				System.out.println("监控所有被触发的事件:EVENT:" + event.getType());
 
 			}
 		});
-		
-		
+
+		System.out.println("*******************************************************");
+		// 查看根节点的子节点
+		System.out.println("查看根节点的子节点:ls / => " + zk.getChildren("/", true));
+		System.out.println("*******************************************************");
+
 		// 关闭连接
-        zk.close();
+		zk.close();
 
 	}
 
