9f6e3a29a1a1345831052376bd70d17ba4e1d9a3
zhaoshuxue <zhaoshuxue@163.com>
2018-12-26 15:03:35 +0800

    commitByZsx

--- dubbo-spring-cloud/spring-cloud-consumer-consul/src/main/java/com/zsx/controller/ConsumerController.java
+++ dubbo-spring-cloud/spring-cloud-consumer-consul/src/main/java/com/zsx/controller/ConsumerController.java
@@ -1,10 +1,13 @@
 package com.zsx.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.consul.discovery.ConsulDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 public class ConsumerController {
@@ -12,10 +15,19 @@ public class ConsumerController {
     @Autowired
     RestTemplate restTemplate;
 
+    @Autowired
+    ConsulDiscoveryClient consulDiscoveryClient;
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String add() {
-        String object = this.restTemplate.getForObject("https://consul-server-producer/add?a=10&b=20", String.class);
+
+        List<String> services = consulDiscoveryClient.getServices();
+        System.out.println();
+        for (String service : services) {
+            System.out.println(service);
+        }
+
+        String object = this.restTemplate.getForObject("http://localhost:8082/add?a=10&b=20", String.class);
         return object;
     }
 }
