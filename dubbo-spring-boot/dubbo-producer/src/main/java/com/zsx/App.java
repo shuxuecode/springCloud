7837b554d54161f2ad60730467269f7371944545
zhaoshuxue <zhaoshuxue@163.com>
2021-06-17 00:48:03 +0800

    commitByZsx

---
+++ dubbo-spring-boot/dubbo-producer/src/main/java/com/zsx/App.java
@@ -0,0 +1,15 @@
+package com.zsx;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubboConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableDubboConfig
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
---
+++ dubbo-spring-boot/dubbo-producer/src/main/java/com/zsx/service/impl/DemoServiceImpl.java
@@ -0,0 +1,17 @@
+package com.zsx.service.impl;
+
+import com.zsx.service.DemoService;
+import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@DubboService
+public class DemoServiceImpl implements DemoService {
+
+    @Override
+    public String name(String name) {
+        return UUID.randomUUID().toString();
+    }
+}
