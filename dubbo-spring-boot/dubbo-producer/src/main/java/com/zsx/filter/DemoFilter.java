df02f9b1c95cf1bef564a96aa78c831171b56ab5
zhaoshuxue <zhaoshuxue@163.com>
2021-07-07 22:14:23 +0800

    commitByZsx

--- dubbo-spring-boot/dubbo-producer/src/main/java/com/zsx/filter/DemoFilter.java
+++ dubbo-spring-boot/dubbo-producer/src/main/java/com/zsx/filter/DemoFilter.java
@@ -6,8 +6,13 @@ import org.apache.dubbo.rpc.*;
  * 自定义过滤器
  */
 public class DemoFilter implements Filter {
+
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
-        return null;
+        System.out.println("执行了自定义过滤器");
+
+        // todo
+        Result result = invoker.invoke(invocation);
+        return result;
     }
 }
