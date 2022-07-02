c60da89f038233e25554d8398f1b4646d038ecb6
shuxuecode <zhaoshuxue@163.com>
2022-07-02 22:34:17 +0800

    :children_crossing: :closed_lock_with_key: :arrow_up:

--- upload.sh
+++ upload.sh
@@ -24,7 +24,10 @@ git add .
 echo "----------------------------------------------------------------------------"
 echo -e "\033[35m -------------------------------  git commit -m commitByZsx  ---------------- \033[0m"
 echo "----------------------------------------------------------------------------"
-git commit -m commitByZsx
+# git commit -m commitByZsx
+
+emoji=(':art:' ':zap:' ':fire:' ':bug:' ':ambulance:' ':sparkles:' ':memo:' ':rocket:' ':lipstick:' ':tada:' ':white_check_mark:' ':lock:' ':closed_lock_with_key:' ':bookmark:' ':rotating_light:' ':construction:' ':green_heart:' ':arrow_down:' ':arrow_up:' ':pushpin:' ':construction_worker:' ':chart_with_upwards_trend:' ':recycle:' ':heavy_plus_sign:' ':heavy_minus_sign:' ':wrench:' ':hammer:' ':globe_with_meridians:' ':pencil2:' ':poop:' ':rewind:' ':twisted_rightwards_arrows:' ':package:' ':alien:' ':truck:' ':page_facing_up:' ':boom:' ':bento:' ':wheelchair:' ':bulb:' ':beers:' ':speech_balloon:' ':card_file_box:' ':loud_sound:' ':mute:' ':busts_in_silhouette:' ':children_crossing:' ':building_construction:' ':iphone:' ':clown_face:' ':egg:' ':see_no_evil:' ':camera_flash:' ':alembic:' ':mag:' ':label:' ':seedling:' ':triangular_flag_on_post:' ':goal_net:' ':dizzy:' ':wastebasket:' ':passport_control:' ':adhesive_bandage:' ':monocle_face:' ':coffin:' ':test_tube:' ':necktie:' ':stethoscope:' ':bricks:' ':technologist:' ':money_with_wings:')
+git commit -m "${emoji[$(($RANDOM % ${#emoji[@]}))]} ${emoji[$(($RANDOM % ${#emoji[@]}))]} ${emoji[$(($RANDOM % ${#emoji[@]}))]}"
 
 echo "----------------------------------------------------------------------------"
 echo -e "\033[36m -------------------------------  git push  --------------------------------- \033[0m"
